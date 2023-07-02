b66aa71e9b68ca84c2f0797055d8277ce5956556
adnanabbasi <adnan_abbasi@yahoo.com>
2023-07-02 00:21:33 -0400

    Ch. 2: Using Node.js, Express JS, and npm Packages

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 8080
+app.listen(port, () => {
+
+})
+app.get('/', (request, response) => {
+  response.json({
+    "Hosting": "Kinsta"
+  })
+})
+console.log(`Listening on ${port}`)
